7c25e3029ff641a86c38e7f6977268e9b94c4a9a
jonatangroncode <jonatan.gron.code@hotmail.com>
2023-12-28 00:11:45 +0100

    todotaskpostandget

---
+++ Todos.MiniAPI/Controllers/TodoTaskController.cs
@@ -0,0 +1,55 @@
+using Microsoft.AspNetCore.Mvc;
+using Todos.Data.Contexts;
+using Todos.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace Todos.MiniAPI.Controllers
+{
+    [ApiController]
+    [Route("apiTodo/[controller]")]
+    public class TodoTasksController : ControllerBase
+    {
+        private readonly TodosDbContext _context;
+
+        public TodoTasksController(TodosDbContext context)
+        {
+            _context = context;
+        }
+
+  [HttpPost("todotasks")]
+public IActionResult CreateTodoTask(string description, string category, int todoId)
+{
+    if (string.IsNullOrWhiteSpace(description))
+    {
+        ModelState.AddModelError("Description", "The Description field is required.");
+        return BadRequest(ModelState);
+    }
+
+    var newTodoTask = new TodoTasks
+    {
+        Description = description,
+        Category = category,
+        TodoId = todoId
+    };
+
+    _context.Tasks.Add(newTodoTask);
+    _context.SaveChanges();
+
+    return NoContent();
+}
+
+[HttpGet("getTodoTasks")]
+public IActionResult GetAllTodoTasks()
+{
+    var todoTasks = _context.Tasks.ToList();
+
+    if (todoTasks == null || todoTasks.Count == 0)
+    {
+        return NotFound();
+    }
+
+    return Ok(todoTasks);
+}
+    }
+}
