4bb7a565f43d31262503d614a2631db653ccd5a2
jonatangroncode <jonatan.gron.code@hotmail.com>
2023-12-28 15:23:58 +0100

    delete a task works

--- Todos.MiniAPI/Controllers/TodoTaskController.cs
+++ Todos.MiniAPI/Controllers/TodoTaskController.cs
@@ -17,39 +17,81 @@ namespace Todos.MiniAPI.Controllers
             _context = context;
         }
 
-  [HttpPost("todotasks")]
-public IActionResult CreateTodoTask(string description, string category, int todoId)
-{
-    if (string.IsNullOrWhiteSpace(description))
-    {
-        ModelState.AddModelError("Description", "The Description field is required.");
-        return BadRequest(ModelState);
+        [HttpPost("todotasks")]
+        public IActionResult CreateTodoTask(string description, string category, int todoId)
+        {
+            if (string.IsNullOrWhiteSpace(description))
+            {
+                ModelState.AddModelError("Description", "The Description field is required.");
+                return BadRequest(ModelState);
+            }
+
+            var newTodoTask = new TodoTasks
+            {
+                Description = description,
+                Category = category,
+                TodoId = todoId
+            };
+
+            _context.Tasks.Add(newTodoTask);
+            _context.SaveChanges();
+
+            return NoContent();
+        }
+
+        [HttpGet("getTodoTasks")]
+        public IActionResult GetAllTodoTasks()
+        {
+            var todoTasks = _context.Tasks.ToList();
+
+            if (todoTasks == null || todoTasks.Count == 0)
+            {
+                return NotFound();
+            }
+
+            return Ok(todoTasks);
+        }
+
+        [HttpDelete("deleteTodoTask/{id}")]
+        public IActionResult DeleteTodoTask(int id)
+        {
+            var todoTask = _context.Tasks.Find(id);
+
+            if (todoTask == null)
+            {
+                return NotFound();
+            }
+
+            _context.Tasks.Remove(todoTask);
+            _context.SaveChanges();
+
+            return NoContent();
+        }
+
+        [HttpPut("updateTodoTask/{id}")]
+        public IActionResult UpdateTodoTask(int id, [FromBody] TodoTasks updatedTodoTask)
+        {
+            if (id != updatedTodoTask.Id)
+            {
+                return BadRequest();
+            }
+
+            var existingTodoTask = _context.Tasks.Find(id);
+
+            if (existingTodoTask == null)
+            {
+                return NotFound();
+            }
+
+            // Update properties of the existingTodoTask with values from updatedTodoTask
+            existingTodoTask.Description = updatedTodoTask.Description;
+            existingTodoTask.Category = updatedTodoTask.Category;
+
+            _context.SaveChanges();
+
+            return NoContent();
+        }
     }
 
-    var newTodoTask = new TodoTasks
-    {
-        Description = description,
-        Category = category,
-        TodoId = todoId
-    };
 
-    _context.Tasks.Add(newTodoTask);
-    _context.SaveChanges();
-
-    return NoContent();
-}
-
-[HttpGet("getTodoTasks")]
-public IActionResult GetAllTodoTasks()
-{
-    var todoTasks = _context.Tasks.ToList();
-
-    if (todoTasks == null || todoTasks.Count == 0)
-    {
-        return NotFound();
-    }
-
-    return Ok(todoTasks);
-}
-    }
 }
